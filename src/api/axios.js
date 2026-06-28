b75e0daf6d5a6510a6dff36f9f28f49902da8ffe
abdellrahmanHq <hanaqtahabdellrahman@gmail.com>
2026-06-28 18:23:03 +0300

    Update API endpoint base URL to live production domain

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
-const API=axios.create({
-    baseURL:'http://localhost:3000'
+
+const API = axios.create({
+  baseURL: 'https://school-api-8xfg.onrender.com',
 });
- API.interceptors.request.use((config) => {
 
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-    const token=localStorage.getItem('token');
-    if(token){
-        config.headers.Authorization='Bearer ${token}';
-    }
-    return config;   
- });
- export default API;
+export default API;
